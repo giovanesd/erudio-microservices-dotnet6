b1e287aba7d54ec7b1c39b478edbf201b891c8a0
Giovane Santos de Deus <giovanesd@gmail.com>
2022-12-26 21:26:57 -0300

    Creating MS ProductAPI access to database. Mapping database entities.

--- GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -1,8 +1,15 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+var connection = builder.Configuration["DBConnection:DBConnectionString"];
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql( connection,
+    new MySqlServerVersion(new Version(8, 0, 31))));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
